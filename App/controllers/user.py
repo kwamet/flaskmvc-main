1aff803b6efcca6802550d9f97933c9558f34016
Kushraj Pollard <kushrajpollard@gmail.com>
2023-10-10 19:56:17 -0400

    Update user.py
    Added necessary comments

--- App/controllers/user.py
+++ App/controllers/user.py
@@ -1,21 +1,26 @@
 from App.models import User
 from App.database import db
 
+# Creates a new user
 def create_user(username, password):
     newuser = User(username=username, password=password)
     db.session.add(newuser)
     db.session.commit() 
     return newuser
 
+# Gets the desired user given their username
 def get_user_by_username(username):
     return User.query.filter_by(username=username).first()
 
+# Gets the desired user given their id
 def get_user(id):
     return User.query.get(id)
 
+# Gets all created users
 def get_all_users():
     return User.query.all()
 
+# Gets all created users in a JSON object
 def get_all_users_json():
     users = User.query.all()
     if not users:
@@ -23,6 +28,7 @@ def get_all_users_json():
     users = [user.get_json() for user in users]
     return users
 
+# Updates a user given selected parameters
 def update_user(id, username):
     user = get_user(id)
     if user:
